b4f949f3b57f940408841bcebf06010ed900a802
glacovic <196379883+glacovic@users.noreply.github.com>
2026-02-03 18:02:51 -0500

    Docs and fix player moving off window

--- start_game.py
+++ start_game.py
@@ -1,22 +1,20 @@
 import pygame
 pygame.init()
+pygame.display.set_caption("Haunted Meadow Brook") #title of game
 
-win = pygame.display.set_mode((1000, 750))
+#set width and height of window
+width = 1000
+height = 750
+win = pygame.display.set_mode((width, height))
 
-pygame.display.set_caption("Haunted Meadow Brook")
+#load grizzly mascot player
+player = pygame.image.load("photos\\grizzly_photos\\grizzly_mascot_pixel.png")
+player_rect = player.get_rect(center = (0, 0)) #player starts at (x, y)
+player = pygame.transform.scale(player, (150, 200)) #player scale to size (x, y)
+vel = 10 #set velocity of player movement
 
-player_image = pygame.image.load("photos\\grizzly_photos\\grizzly_mascot_pixel.png")
-
-player_rect = player_image.get_rect(center = (350, 350))
-
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 10
-
+#run game
 run = True
-
 while run:
     pygame.time.delay(100)
     
@@ -24,22 +22,29 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             
+    #left, right, up, down movement based on keys pressed
     keys = pygame.key.get_pressed()
-    
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         player_rect.x -= vel
-        
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         player_rect.x += vel
-        
     if keys[pygame.K_w] or keys[pygame.K_UP]:
-        player_rect.y -= vel
-        
+        player_rect.y -= vel 
     if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         player_rect.y += vel
+        
+    #keep player inside edges of window
+    if player_rect.right > width:
+        player_rect.right = width
+    if player_rect.left < 0:
+        player_rect.left = 0
+    if player_rect.bottom > height:
+        player_rect.bottom = height
+    if player_rect.top < 0:
+        player_rect.top = 0
     
     win.fill((0,0,0))  
-    win.blit(player_image, player_rect)     
+    win.blit(player, player_rect)     
     pygame.display.update()
 
 pygame.quit()
